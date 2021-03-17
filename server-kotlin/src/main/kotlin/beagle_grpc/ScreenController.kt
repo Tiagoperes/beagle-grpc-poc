77d8e55d76f883c6dd07c6f6b58d34a7148a6afd
Tiago Peres França <tiago.franca@zup.com.br>
2021-03-17 12:27:00 -0300

    properties in proto message + headers

--- server-kotlin/src/main/kotlin/beagle_grpc/ScreenController.kt
+++ server-kotlin/src/main/kotlin/beagle_grpc/ScreenController.kt
@@ -2,24 +2,33 @@ package beagle_grpc
 
 import beagle.Messages
 import beagle.ScreenControllerGrpcKt
-import br.com.zup.beagle.widget.layout.ScreenBuilder
+import br.com.zup.beagle.serialization.jackson.BeagleSerializationUtil
+import com.fasterxml.jackson.core.JacksonException
+import io.grpc.Status
+import io.grpc.StatusException
+
+// fixme: can we have a type here instead of returning Any?
+typealias ViewBuilder = (parameters: Map<String, Any>, headers: Map<String, String>) -> Any
 
 open class ScreenController : ScreenControllerGrpcKt.ScreenControllerCoroutineImplBase() {
-    private val builders = HashMap<String, ScreenBuilder>()
-
-    protected fun addScreen(name: String, builder: ScreenBuilder) {
-        builders[name] = builder
-    }
-
-    protected fun addScreens(definitions: Map<String, ScreenBuilder>) {
-        definitions.forEach() {
-            builders[it.key] = it.value
-        }
+    open suspend fun getScreenByName(name: String, parameters: Map<String, Any>, headers: Map<String, String>): Any {
+        throw StatusException(
+            Status.UNIMPLEMENTED.withDescription(
+                "Method ScreenController.getScreenByName is unimplemented"
+            )
+        )
     }
 
     override suspend fun getScreen(request: Messages.ScreenRequest): Messages.ViewNode {
-        val builder = builders[request.name] ?: throw Error("Screen with name ${request.name} doesn't exist.")
-        val view = builder.build()
+        val mapper = BeagleSerializationUtil.beagleObjectMapper()
+        val parameters: Map<String, Any> = try {
+            mapper.readValue(request.parameters, MutableMap::class.java) as MutableMap<String, Any>
+        } catch (err: JacksonException) {
+            mapOf()
+        }
+        // todo: get the real headers
+        val headers = HashMap<String, String>()
+        val view = getScreenByName(request.name, parameters, headers)
         return asGrpcView(view)
     }
 }
